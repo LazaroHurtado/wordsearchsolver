16fde5042ac86560a7e61ee252af6d425d298ecb
LazaroHurtado <hurtadolazaro12@gmail.com>
2020-04-10 16:30:38 -0400

    removed strokes added lowercase

--- solvepuzzle.py
+++ solvepuzzle.py
@@ -8,29 +8,45 @@ class solvethepuzzle:
         self.first = self.get_first(words)
         self.last = self.get_last(words)
         self.directions = [(0, 1), (1, 0), (1, -1), (1, 1)]
-        self.strokes = {(0, 1):"-", (1, 0):"|", (1, -1):"/", (1, 1):"\\"}
         self.solve()
         showpuzzle.showpuzzle(self.puzzle)
 
-    def dfs(self, direction, word, puzzle, curr, position, i):
+    def dfs(self, direction, word, puzzle, curr, position, seen):
         if curr == word:
-            self.puzzle = puzzle
-            return
+            print(seen)
+            for location in seen:
+                row, col = location
+                self.puzzle[row][col] = (self.puzzle[row][col]).lower()
+            showpuzzle.showpuzzle(puzzle)
+            return puzzle
         if len(curr) >= len(word):
             return
-        row, col = position
+
         max_row = len(puzzle)-1
         max_col = len(puzzle[0])-1
+        row, col = position
         if row < 0 or col < 0 or row > max_row or col > max_col:
             return
+
+        index = len(curr)
+        curr_reverse = curr[::-1]
+        word_reverse = word[::-1]
+        if curr != word[:index] and curr_reverse != word_reverse[index+1:]:
+            return
+
+        seen.append(position)
         curr_letter = self.puzzle[row][col]
         curr += curr_letter
-        if curr != word[:len(curr)] and curr != word[len(curr)+1:]:
-            return
-        puzzle[row][col] = self.strokes[direction]
         dir_row, dir_col = direction
         new_position = (row + dir_row, col + dir_col)
-        self.dfs(direction, word, puzzle, curr, new_position, i)
+        self.dfs(direction, word, puzzle, curr, new_position, seen)
+
+    def remove_caps(self, locations):
+        print(locations)
+        for location in locations:
+            row, col = location
+            #puzzle[row][col] = (puzzle[row][col]).upper()
+        return
 
     def solve(self):
         for row in range(len(self.puzzle)):
@@ -47,13 +63,11 @@ class solvethepuzzle:
             words = self.first[letter]
         else:
             words = self.last[letter]
-        i = 0
         row, col = position
         temp_puzzle = copy.deepcopy(self.puzzle)
         for word in words:
             for direction in self.directions:
-                self.dfs(direction, word, temp_puzzle, "", position, i)
-                i+=1
+                self.dfs(direction, word, temp_puzzle, "", position, [])
 
     def get_first(self, words):
         first_letters = collections.defaultdict(list)
